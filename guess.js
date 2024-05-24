331daaa39959c4c63b2ac11d867b02c34c1515b5
yeshwanthsai123 <113962518+yeshwanthsai123@users.noreply.github.com>
2024-05-24 12:56:20 +0530

    Update guess.js

--- guess.js
+++ guess.js
@@ -1,15 +1,12 @@
 let x = Math.random()*100;
 let y = Math.round(x);
 let useranswer = document.getElementById("userInput");
-
-
 function checkGuess(){
-let inputted = parseInt(userInput.value);
-if (inputted === y) {
+if (parseInt(userInput.value) === y) {
   gameResult.textContent = "You got it!"
-  } else if (inputted > y) {
+  } else if (parseInt(userInput.value) > y) {
     gameResult.textContent = "Too high!"
-    } else if (inputted < y) {
+    } else if (parseInt(userInput.value) < y) {
       gameResult.textContent = "Too low!"
       }
 }
